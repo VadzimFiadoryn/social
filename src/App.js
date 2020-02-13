5ab1e5551d181e5b2ca7c834bf2a6e4a171799fa
Vadzim <vadzimfiadoryn@gmail.com>
2020-02-13 16:20:33 +0300

    less 11 1step

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+
+
 const App = () => {
-  return (
-    <div className="App">    
-    <Header/>
-    <Technologies/>
-    </div>
-  );
+    return (
+        <div>
+            <header className='app-wrapper'>
+                <img className='headerImg'
+                     src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Umbrella_Corporation_logo.svg/800px-Umbrella_Corporation_logo.svg.png'/>
+            </header>
+            <div className='blockNavCont'>
+                <nav className='nav'>
+                    <div>
+
+                    </div>
+                </nav>
+                <div className='content'>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 export default App;
