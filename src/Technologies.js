9dbff4d7e426b0e5dfc5f2bc36e4f04edeec0cc8
Vadzim <vadzimfiadoryn@gmail.com>
2020-02-13 14:39:13 +0300

    1-6 les

---
+++ src/Technologies.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
+// eslint-disable-next-line no-unused-vars
+const Technologies = () =>{
+    return(
+        
+    <div>
+     <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>   
+    </div>
+    );
+} 
+
+export default Technologies;
---
+++ src/TodoListHeader.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">    
+    Simple Html
+      
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>
+    
+    </div>
+  );
+}
+
+export default App;
