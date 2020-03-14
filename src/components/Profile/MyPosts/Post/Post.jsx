3c51d6f4efc9bc04afcb0c7d5b1f2812af26b851
Vadzim <vadzimfiadoryn@gmail.com>
2020-03-14 21:06:30 +0300

    fix color

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import s from './Post.module.css';
 
 const Post = (props) => {
-    console.log(props.message);
   return (
     <div className={s.item}>
       <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
-        {props.messege}
+        { props.message }
           <div>
-        <span>like = {props.like}</span>
+        <span>like</span> { props.likesCount }
       </div>
     </div>
   )
-
 }
 
 export default Post;
