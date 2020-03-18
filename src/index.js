1d91583096dcd9d3527857a6e2500449ad1f19fa
Vadzim <vadzimfiadoryn@gmail.com>
2020-03-18 04:58:57 +0300

    35 start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./render";
+//import {rerenderEntireTree} from "./render";
 import state from "./redux/state";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText,getNewPostText,updateNewDialogsText,addMessage} from './redux/state';
+import {BrowserRouter} from "react-router-dom";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost}
+                 updateNewPostText={updateNewPostText}
+                 getNewPostText={getNewPostText}
+                // for dialog item
+                 updateNewDialogsText={updateNewDialogsText}
+                 addMessage = {addMessage}
+            />
+
+        </BrowserRouter>, document.getElementById('root'));
+};
 
 rerenderEntireTree(state);
 
--- src/render.js
+++ src/render.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {addPost, updateNewPostText,getNewPostText,updateNewDialogsText,addMessage} from './redux/state';
-import {BrowserRouter} from "react-router-dom";
-
-export let rerenderEntireTree = (state) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <App state={state} addPost={addPost}
-                 updateNewPostText={updateNewPostText}
-                 getNewPostText={getNewPostText}
-                 // for dialog item
-                 updateNewDialogsText={updateNewDialogsText}
-                 addMessage = {addMessage}
-            />
-
-        </BrowserRouter>, document.getElementById('root'));
-};
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import {addPost, updateNewPostText,getNewPostText,updateNewDialogsText,addMessage} from './redux/state';
+// import {BrowserRouter} from "react-router-dom";
+//
+// export let rerenderEntireTree = (state) => {
+//     ReactDOM.render(
+//         <BrowserRouter>
+//             <App state={state} addPost={addPost}
+//                  updateNewPostText={updateNewPostText}
+//                  getNewPostText={getNewPostText}
+//                  // for dialog item
+//                  updateNewDialogsText={updateNewDialogsText}
+//                  addMessage = {addMessage}
+//             />
+//
+//         </BrowserRouter>, document.getElementById('root'));
+// };
